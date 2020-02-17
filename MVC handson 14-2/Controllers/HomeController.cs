87c9e26ba354bb4847fa14c5254c33dcef2425d3
vihaariBot <60810969+vihaariBot@users.noreply.github.com>
2020-02-17 09:37:54 +0530

    Add files via upload

---
+++ MVC handson 14-2/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvchandson14_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            SelectListItem[] skills = new SelectListItem[2];
+            skills[0] = new SelectListItem() { Text = "c", Value = "0" };
+            skills[1] = new SelectListItem() { Text = "java", Value = "2" };
+            ViewBag.skills = skills;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MVC handson 14-2/Controllers/submitController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvchandson14_2.Controllers
+{
+    public class submitController : Controller
+    {
+        // GET: submit
+        public ActionResult data(string rname,int skill,DateTime sdate,DateTime edate)
+        {
+            elhanEntities1 e = new elhanEntities1();
+            Table_1 t = new Table_1();
+            t.name = rname;
+            t.skill = skill;
+            t.startdate = sdate;
+            t.enddate = edate;
+            e.Table_1.Add(t);
+            e.SaveChanges();
+            
+            return View();
+        }
+    }
+}
