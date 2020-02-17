7f3d0b60e0bf3d52168aee71db28b03bf68bf24c
vihaariBot <60810969+vihaariBot@users.noreply.github.com>
2020-02-17 09:39:20 +0530

    Add files via upload

---
+++ MVC trails/Controllers/newController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvctrails.Controllers
+{
+    public class newController : Controller
+    {
+        // GET: new
+        public ActionResult signin()
+        {
+            return View();
+        }
+    }
+}
