9c3ce877aea006799c1e6a7fda85a0da8f91d4a6
vihaariBot <60810969+vihaariBot@users.noreply.github.com>
2020-02-17 09:27:50 +0530

    Add files via upload

---
+++ mvchandson-mailapplication15-2/Controllers/checkController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvchandson_mailapplication15_2.Models;
+
+namespace mvchandson_mailapplication15_2.Controllers
+{
+    public class checkController : Controller
+    {
+        // GET: check
+        public ActionResult signin(string username,string password)
+        {
+            loginEntities1 e = new loginEntities1();
+            var idd = from t in e.logins where (t.username == username && t.password == password) select t.id;
+            var id = idd.FirstOrDefault();
+            var data = from t in e.maildetails where (t.userid == id) select t;
+            List<mvchandson_mailapplication15_2.Models.maildata> maillist = new List<mvchandson_mailapplication15_2.Models.maildata>();
+            foreach (var t in data)
+            {
+                mvchandson_mailapplication15_2.Models.maildata mail = new mvchandson_mailapplication15_2.Models.maildata();
+                mail.mailid = t.mailid;
+                mail.userid = t.userid;
+                mail.mailfrom =t.mailfrom;
+                mail.subject = t.subject;
+                mail.recievedate = t.recievedate;
+                mail.messge = t.message;
+                maillist.Add(mail);
+            }
+
+            return View(maillist);
+        }
+        public ActionResult viewmessage(int mid)
+        {
+            loginEntities1 e = new loginEntities1();
+            var data = from t in e.maildetails where (t.userid == mid) select t;
+            var mdata = data.FirstOrDefault();
+            mvchandson_mailapplication15_2.Models.maildata mail = new mvchandson_mailapplication15_2.Models.maildata();
+            mail.mailid = mdata.mailid;
+            mail.userid = mdata.userid;
+            mail.mailfrom = mdata.mailfrom;
+            mail.subject = mdata.subject;
+            mail.recievedate = mdata.recievedate;
+            mail.messge = mdata.message;
+            return View(mail);
+        }
+    }
+}
---
+++ mvchandson-mailapplication15-2/Models/maildata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvchandson_mailapplication15_2.Models
+{
+    public class maildata
+    {
+        public int mailid { get; set; }
+        public int userid { get; set; }
+        public string mailfrom { get; set; }
+        public string subject { get; set; }
+        public DateTime recievedate { get; set; }
+        public string messge { get; set; }
+    }
+}
