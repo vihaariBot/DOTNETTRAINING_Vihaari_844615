feb0f5ec7f0bb790d04211d37596be1a9e2b262d
vihaariBot <60810969+vihaariBot@users.noreply.github.com>
2020-02-17 09:43:25 +0530

    Add files via upload

---
+++ DAL Handson/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dalhandson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dataaccess ds = new dataaccess();
+            string username = Console.ReadLine();
+            string password = Console.ReadLine();
+            int id=ds.login(username, password);
+            Console.WriteLine(id);
+            List<data> d= ds.access(id);
+            for(int i=0;i<d.Count;i++)
+            {
+                Console.Write(d[i].empid);
+                Console.WriteLine(d[i].empname);
+
+            }
+        }
+    }
+}
---
+++ DAL Handson/dataaccess.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace dalhandson
+{
+    class dataaccess
+    {
+        public int login(string user, string pass)
+        {
+            string str = "Data Source=BLT10120\\SQLEXPRESS;Initial Catalog= login;Integrated Security=True";
+            SqlConnection con = new SqlConnection(str);
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "select id from login where username='"+user+"' and password='"+pass+"'";
+            cmd.Connection = con;
+            int id = int.Parse(cmd.ExecuteScalar().ToString());
+            con.Close();
+            return id;
+        }
+        public List<data> access (int id)
+        {
+            string str = "Data Source=BLT10120\\SQLEXPRESS;Initial Catalog= login;Integrated Security=True";
+            SqlConnection con = new SqlConnection(str);
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "select id,name from data where mid='" + id +"'";
+            cmd.Connection = con;
+            SqlDataReader rd = cmd.ExecuteReader();
+            List<data> d = new List<data>();
+            while(rd.Read())
+            {
+                int i = int.Parse(rd[0].ToString());
+                string name = rd[1].ToString();
+                data dd = new data(i, name);
+                d.Add(dd);
+            }
+            return d;
+
+        }
+       
+    }
+
+    public class data
+    {
+        public int empid;
+        public string empname;
+        public data(int emp, string name)
+        {
+            empid = emp;
+            empname = name;
+        }
+    }
+}
