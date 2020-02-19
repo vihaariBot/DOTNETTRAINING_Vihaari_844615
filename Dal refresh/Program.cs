80d9e34ed7487578df0aa3a38ca5d3e0792e0a7e
vihaariBot <60810969+vihaariBot@users.noreply.github.com>
2020-02-19 09:57:03 +0530

    Add files via upload

---
+++ Dal refresh/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dal_refresh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string uname = Console.ReadLine();
+            string pass = Console.ReadLine();
+            dal obj = new dal();
+            int id = obj.loginvalid(uname,pass);
+            Console.WriteLine(id);
+            data d = obj.retrieve(id);
+            Console.WriteLine(d.name);
+            Console.WriteLine(d.mid);
+        }
+    }
+}
---
+++ Dal refresh/dal.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace dal_refresh
+{
+    class dal
+    {
+        public int loginvalid(string uname,string pass)
+        {
+            string constr = "Data Source=BLT10120\\SQLEXPRESS;Initial Catalog=login;Integrated Security=true";
+            SqlConnection con = new SqlConnection(constr);
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "select id from login where username='"+uname+"' and password='"+pass+"'";
+            cmd.Connection = con;
+            int val=int.Parse(cmd.ExecuteScalar().ToString());
+            con.Close();
+            return val;
+        
+           
+
+        }
+        public data retrieve(int id)
+        {
+            data d = new data();
+            string constr = "Data Source=BLT10120\\SQLEXPRESS;Initial Catalog=login;Integrated Security=true";
+            SqlConnection con = new SqlConnection(constr);
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "select name,mid from data where id=" + id;
+            cmd.Connection=con;
+            SqlDataReader reader = cmd.ExecuteReader();
+            while(reader.Read())
+            {
+                d.name = reader[0].ToString();
+                d.mid = int.Parse(reader[1].ToString());
+            }
+            return d;
+        }
+       
+    }
+    public class data
+    {
+        public string name;
+        public int mid;
+    }
+}
