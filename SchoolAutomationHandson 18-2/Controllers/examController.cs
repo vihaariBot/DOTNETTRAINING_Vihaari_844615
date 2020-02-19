9d0c079254bc4ec4f6859920f2fd30afe6903740
vihaariBot <60810969+vihaariBot@users.noreply.github.com>
2020-02-19 10:02:18 +0530

    Add files via upload

---
+++ SchoolAutomationHandson 18-2/Controllers/examController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace schoolautomationhandson_18_2.Controllers
+{
+    public class examController : Controller
+    {
+        // GET: exam
+        public ActionResult add()
+        {
+            return View();
+        }
+        public ActionResult addtodb(int examid,string ename, int scount,int year)
+        {
+            automatedexamsEntities1 e = new automatedexamsEntities1();
+            exam ex = new exam();
+            ex.eid = examid;
+            ex.ename = ename;
+            ex.subjectcount = scount;
+            ex.year = year;
+            e.exams.Add(ex);
+            e.SaveChanges();
+            return View();
+            
+        }
+    }
+}
---
+++ SchoolAutomationHandson 18-2/Controllers/schoolController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace schoolautomationhandson_18_2.Controllers
+{
+    public class schoolController : Controller
+    {
+        // GET: school
+        public ActionResult add()
+        {
+            return View();
+        }
+
+        public ActionResult addtodb(int sid,string sname,int scount,int eid,string city)
+        {
+            automatedexamsEntities1 e = new automatedexamsEntities1();
+            school sc = new school();
+            bool flag=false;
+           foreach(var v in e.exams)
+            {
+                if(v.eid==eid)
+                {
+                   flag = true;
+                }
+            }
+           if(flag)
+            {
+                sc.sid = sid;
+                sc.sname = sname;
+                sc.count = scount;
+                sc.examid = eid;
+                sc.city = city;
+                e.schools.Add(sc);
+                e.SaveChanges();
+            }
+           
+            return View();
+        }
+    }
+}
